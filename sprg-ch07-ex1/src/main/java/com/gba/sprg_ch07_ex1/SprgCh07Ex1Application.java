6015f8c922a21d0cede3eac9ba72a9ad3eeeef23
ceveloper7 <cevabarcvilla@gmail.com>
2026-04-13 21:00:58 +0000

    Spring start here - Chapter 7 - Spring boot MVC

---
+++ sprg-ch07-ex1/src/main/java/com/gba/sprg_ch07_ex1/SprgCh07Ex1Application.java
@@ -0,0 +1,13 @@
+package com.gba.sprg_ch07_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprgCh07Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprgCh07Ex1Application.class, args);
+	}
+
+}
---
+++ sprg-ch07-ex1/src/test/java/com/gba/sprg_ch07_ex1/SprgCh07Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gba.sprg_ch07_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprgCh07Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
