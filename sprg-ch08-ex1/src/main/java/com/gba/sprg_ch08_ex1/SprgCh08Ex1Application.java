839c15d8509590e1c11648b90182768a7929df07
ceveloper7 <cevabarcvilla@gmail.com>
2026-04-20 21:46:04 +0000

    Spring start here: chapter 8 - Implentando web apps con vista dinamica

---
+++ sprg-ch08-ex1/src/main/java/com/gba/sprg_ch08_ex1/SprgCh08Ex1Application.java
@@ -0,0 +1,13 @@
+package com.gba.sprg_ch08_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprgCh08Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprgCh08Ex1Application.class, args);
+	}
+
+}
