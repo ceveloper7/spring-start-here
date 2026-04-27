78765ded682b3c27d291e9dc8f412a2e03802c9f
ceveloper7 <cevabarcvilla@gmail.com>
2026-04-27 23:41:08 +0000

    Spring start here - chapter 8 - QueryParam sample

---
+++ sprg-ch08-ex2/src/main/java/com/gba/sprg_ch08_ex2/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.gba.sprg_ch08_ex2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @RequestMapping("/home")
+    public String home(@RequestParam String username, @RequestParam String color, Model page){
+        page.addAttribute("username", username);
+        page.addAttribute("color", color);
+
+        return "home.html";
+    }
+}
---
+++ sprg-ch08-ex2/src/test/java/com/gba/sprg_ch08_ex2/SprgCh08Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gba.sprg_ch08_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprgCh08Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
