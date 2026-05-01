3fbbe26d9f31a985ae68ab0339bbd44f5964fe36
ceveloper7 <cevabarcvilla@gmail.com>
2026-05-01 21:39:06 +0000

    Spring - chapter 8 - Using Path Variable

---
+++ sprg-ch08-ex3/src/main/java/com/gba/sprg_ch08_ex3/SprgCh08Ex3Application.java
@@ -0,0 +1,13 @@
+package com.gba.sprg_ch08_ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprgCh08Ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprgCh08Ex3Application.class, args);
+	}
+
+}
---
+++ sprg-ch08-ex3/src/test/java/com/gba/sprg_ch08_ex3/SprgCh08Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gba.sprg_ch08_ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprgCh08Ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
