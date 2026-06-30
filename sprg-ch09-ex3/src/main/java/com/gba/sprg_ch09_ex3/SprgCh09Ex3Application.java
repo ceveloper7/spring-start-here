a3b289f6dbb34ab3ada047f749ed249537b2fa38
ceveloper7 <cevabarcvilla@gmail.com>
2026-06-30 19:09:39 +0000

    Spring start here - chapter 9 - Application Scope Sample

---
+++ sprg-ch09-ex3/src/main/java/com/gba/sprg_ch09_ex3/SprgCh09Ex3Application.java
@@ -0,0 +1,13 @@
+package com.gba.sprg_ch09_ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprgCh09Ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprgCh09Ex3Application.class, args);
+	}
+
+}
---
+++ sprg-ch09-ex3/src/test/java/com/gba/sprg_ch09_ex3/SprgCh09Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gba.sprg_ch09_ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprgCh09Ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
